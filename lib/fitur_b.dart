f6affd1d82cfe11a112215c1fec17cc96ea9930f
hendriari <hendriariwibowo21@gmail.com>
2022-08-23 23:19:38 +0700

    menambah navigasi untuk page fitur b

---
+++ lib/fitur_b.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FiturB extends StatelessWidget {
+  const FiturB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ini halaman Fitur B'),
+      ),
+      body: const Center(
+        child: Text('ini halaman fitur B'),
+      ),
+    );
+  }
+}
